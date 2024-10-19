6e9088c62db7f063c9fa4f7ee18caf362f43726c
Javaria Shaukat <javaria.y2b@gmail.com>
2024-10-20 00:57:57 +0500

    Adding a kanban update API completed

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -13,6 +13,9 @@ import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns"
 import { DataKanban } from "./data-kanban";
+import { useCallback } from "react";
+import { TaskStatus } from "../types";
+import { useBulkUpdateTasks } from "../api/use-bulk-update-task";
 
 export const TaskViewSwitcher = () => {
 
@@ -30,6 +33,8 @@ export const TaskViewSwitcher = () => {
   const workspaceId = useWorkspaceId();
   const { open } = useCreateTaskModal();
 
+  const {mutate: bulkUpdate} = useBulkUpdateTasks()
+
   const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
     workspaceId,
     projectId,
@@ -37,6 +42,16 @@ export const TaskViewSwitcher = () => {
     status,
     dueDate
   });
+
+  const onKanbanChange = useCallback(
+    (tasks: { $id: string; status: TaskStatus; position: number }[]) => {
+     bulkUpdate({
+      json: {tasks},
+     })
+    },
+    [bulkUpdate]
+  );
+  
 
   return (
     <Tabs
@@ -78,7 +93,7 @@ export const TaskViewSwitcher = () => {
             </TabsContent>
 
             <TabsContent value="kanban" className="mt-0">
-              <DataKanban data={tasks?.documents ?? [] } />
+              <DataKanban onChange={onKanbanChange} data={tasks?.documents ?? [] } />
             </TabsContent>
 
             <TabsContent value="calendar" className="mt-0">
